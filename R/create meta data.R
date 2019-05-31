6ef0f8eb8935dc59134282db5777dfbe757fd40b
audhalbritter <audhalbritter.com>
2019-05-31 22:22:32 +0200

    same nitrogen treatment for each block

--- R/create meta data.R
+++ R/create meta data.R
@@ -5,32 +5,41 @@
 library("tidyverse")
 library("writexl")
 
+pn <- . %>% print(n = Inf)
+
 # make meta data
 set.seed(32)
 
-origSite <-  c("Joa", "Lia")
-block <-  c(1,2,3,4,5)
-climate <-  c("A", "W")
+origSiteID <-  c("Joa", "Lia")
+blockID <-  c(1,2,3,4,5)
+plotID <- tibble(plotID = rep(1:16, 10))
+warming <-  c("A", "W")
 Nlevel <- c("N1", "N2")
 grazing <-  c("C", "M", "I", "N")
 
-nitrogen <- tibble(nitrogen = replicate(n = 4, sample(1:10, 10), simplify = FALSE) %>% unlist()) 
+#nitrogen <- tibble(nitrogen = replicate(n = 4, sample(1:10, 10), simplify = FALSE) %>% unlist())
 
-meta <- crossing(origSite, block, climate, Nlevel) %>% 
-  arrange(origSite, climate, block) %>% 
+nitrogen <- tibble(nitrogen = rep(rep(sample(1:10, 10), each = 2), 2))
+
+meta <- crossing(origSiteID, blockID, warming, Nlevel) %>% 
+  arrange(origSiteID, blockID, Nlevel) %>% 
   bind_cols(nitrogen) %>% 
   select(-Nlevel) %>% 
   crossing(grazing) %>% 
-  mutate(turfID = paste0(origSite, block, climate, nitrogen, grazing))
+  mutate(turfID = paste0(origSiteID, blockID, warming, nitrogen, grazing)) %>% 
+  mutate(destSiteID = case_when(origSiteID == "Lia" & warming == "A" ~ "Lia",
+                                origSiteID == "Lia" & warming == "W" ~ "Joa",
+                                origSiteID == "Joa" & warming == "A" ~ "Joa",
+                                origSiteID == "Joa" & warming == "W" ~ "Vik"))
 
 # randomize the grazing treatments and nitrogen level, within site, climate and block. Natural grazing plots are separate.
 
 ExperimentalDesign <- meta %>% 
   mutate(sorter = if_else(grazing == "N", "out", "in")) %>% 
-  group_by(origSite, block, climate, sorter) %>%
+  group_by(blockID, destSiteID, sorter) %>%
   sample_frac() %>% 
-  group_by(origSite, block, climate) %>%
+  group_by(destSiteID, blockID, warming) %>%
   select(-sorter) %>% 
-  mutate(plotID = 1:8)
+  bind_cols(plotID)
 
 write_xlsx(ExperimentalDesign, path = "ExperimentalDesign.xlsx", col_names = TRUE)
