7d807ee744cf317c1eb31cfb7331d0fa51df6ad0
audhalbritter <audhalbritter.com>
2019-06-28 09:05:30 +0200

    Starting script - loading libraries - defining colours etc.

---
+++ R/Load packages.R
@@ -0,0 +1,14 @@
+#### LOAD PACKAGES ####
+
+library("tidyverse")
+library("lubridate")
+library("writexl")
+
+
+# Stuff
+pn <- . %>% print(n = Inf)
+
+
+# Colours
+# The palette with grey:
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
