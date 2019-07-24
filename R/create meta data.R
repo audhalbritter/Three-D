cf3b95d4571260b98d17e40215db85d058e057da
audhalbritter <audhalbritter.com>
2019-07-24 19:08:13 +0200

    delete unused code

--- R/create meta data.R
+++ R/create meta data.R
@@ -62,26 +62,3 @@ ExperimentalDesign <- left_join(
   select(-fence)
 
 #write_xlsx(ExperimentalDesign, path = "ExperimentalDesign_24-7-19.xlsx", col_names = TRUE)
-  
-  
-#Old code
-nitrogen <- tibble(nitrogen = rep(rep(sample(1:10, 10), each = 2), 2))
-meta <- crossing(origSiteID, blockID, warming, Nlevel) %>% 
-  arrange(origSiteID, blockID, Nlevel) %>% 
-  bind_cols(nitrogen) %>% 
-  select(-Nlevel) %>% 
-  crossing(grazing) %>% 
-  mutate(turfID = paste0(origSiteID, blockID, warming, nitrogen, grazing)) %>% 
-  mutate(destSiteID = case_when(origSiteID == "Lia" & warming == "A" ~ "Lia",
-                                origSiteID == "Lia" & warming == "W" ~ "Joa",
-                                origSiteID == "Joa" & warming == "A" ~ "Joa",
-                                origSiteID == "Joa" & warming == "W" ~ "Vik"))
-
-# randomize the grazing treatments and nitrogen level, within site, climate and block. Natural grazing plots are separate.
-ExperimentalDesign <- meta %>% 
-  mutate(sorter = if_else(grazing == "N", "out", "in")) %>% 
-  group_by(blockID, destSiteID, sorter) %>%
-  sample_frac() %>% 
-  group_by(destSiteID, blockID, warming) %>%
-  select(-sorter) %>% 
-  bind_cols(plotID)
