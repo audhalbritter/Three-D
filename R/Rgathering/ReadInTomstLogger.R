9994318ba9de5d1b8bc4fe20dbe682ee053b269f
audhalbritter <audhalbritter.com>
2020-01-07 18:31:40 +0100

    fix data remove failures remove data before installation in the field

--- R/Rgathering/ReadInTomstLogger.R
+++ R/Rgathering/ReadInTomstLogger.R
@@ -9,7 +9,10 @@ source("R/create meta data.R")
 #### CLIMATE DATA ####
 
 # Read in meta data
-meta <- read_excel(path = "data/metaData/Three-D_ClimateLogger_Meta_2019.xlsx", col_names = TRUE)
+meta <- read_excel(path = "data/metaData/Three-D_ClimateLogger_Meta_2019.xlsx", col_names = TRUE, col_types = c("text", "numeric", "text", "text", "date", "date", "text")) %>% 
+  mutate(InitialDate = ymd(InitialDate),
+         InitialDate_Time = ymd_hm(paste(InitialDate, paste(hour(InitialTime), minute(InitialTime), sep = ":"), sep = " ")))
+
 
 ### Read in files
 files <- dir(path = "data/climate tomst/", pattern = "^data.*\\.csv$", full.names = TRUE, recursive = TRUE)
@@ -21,17 +24,30 @@ temp <- map_df(set_names(files), function(file) {
     map_df(~ read_delim(file = file, col_names = FALSE, delim = ";"))
 }, .id = "File") %>% 
   # rename column names
-  rename("ID" = "X1", "Date_Time" = "X2", "Time_zone" = "X3", "SoilTemperature" = "X4", "GroundTemperaure" = "X5", "AirTemperature" = "X6", "RawSoilmoisture" = "X7", "Shake" = "X8", "ErrorFlag" = "X9") %>% 
+  rename("ID" = "X1", "Date_Time" = "X2", "Time_zone" = "X3", "SoilTemperature" = "X4", "GroundTemperature" = "X5", "AirTemperature" = "X6", "RawSoilmoisture" = "X7", "Shake" = "X8", "ErrorFlag" = "X9") %>% 
   mutate(Date_Time = ymd_hm(Date_Time)) %>% 
   # Soil moisture calibration
   #mutate(SoilMoisture = a * RawSoilmoisture^2 + b * RawSoilmoisture + c) %>% 
   # get logger ID
-  mutate(LoggerID = as.numeric(substr(File, nchar(File)-9, nchar(File)-6))) %>% 
+  mutate(LoggerID = substr(File, nchar(File)-9, nchar(File)-6)) %>% 
   left_join(meta, by = "LoggerID")
   
 
+# Fix data
+temp <- temp %>% 
+  # Remove logger failure
+  filter(ErrorFlag == 0) %>% 
+  # Remove data before initial date time
+  group_by(LoggerID) %>% 
+  filter(Date_Time > InitialDate_Time)
 
-  
+temp %>% filter(LoggerID == "5255") %>% select(InitialDate_Time)
+
+temp %>% 
+  ggplot(aes(x = Date_Time, y = AirTemperature, colour = as.factor(LoggerID))) +
+  geom_line() +
+  facet_grid(BlockID ~ Site) +
+  theme(legend.position="none")
   
 
 
