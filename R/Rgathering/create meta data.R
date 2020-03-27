3a1aa6435e8d8be55f293ccf4a207fec89f975ce
audhalbritter <me@audhalbritter.com>
2020-03-27 11:42:47 +0100

    Add nitrogen amount
    Add Chinese data

--- R/Rgathering/create meta data.R
+++ R/Rgathering/create meta data.R
@@ -13,6 +13,9 @@ warming <-  c("A", "W")
 grazing <-  c("C", "M", "I", "N")
 # Nitrogen level needs to be in a certain order
 nitrogen <- tibble(Nlevel = rep(rep(c(1,6,5,3,10,7,4,8,9,2), each = 8), 2))
+# add corresponding N amount in kg per ha and year
+NitrogenDictionary <- tibble(Nlevel = c(1,6,5,3,10,7,4,8,9,2),
+                             Namount_kg_ha_y = c(0, 5, 1, 0, 150, 10, 0.5, 50, 100, 0))
 
 # cross site, block warm and grazing treatment
 meta <- crossing(origSiteID, origBlockID, warming, grazing) %>% 
@@ -77,3 +80,71 @@ metaTurfID <- left_join(
                                 TRUE ~ destSiteID))
 
 #write_xlsx(metaTurfID, path = "metaTurfID_24-7-19.xlsx", col_names = TRUE)
+
+
+
+#########################################################################
+#### CHINA ####
+
+# Create meta data
+# Middle and Top
+origSiteID <-  c("Top", "Middle")
+origBlockID <-  c(1:10)
+origPlotID <- tibble(origPlotID = 1:160)
+warming <-  c("A", "W")
+grazing <-  c("C", "M", "I", "N")
+# Nitrogen level needs to be in a certain order
+nitrogen <- tibble(Nlevel = rep(rep(c(4,1,2,3,6,5,10,9,7,8), each = 8), 2))
+# add corresponding N amount in kg per ha and year
+NitrogenDictionary <- tibble(Nlevel = c(4,1,2,3,6,5,10,9,7,8),
+                             Namount_kg_ha_y = c(0.5,0,0,0,5,1,150,100,10,50))
+
+# cross site, block warm and grazing treatment
+meta <- crossing(origSiteID, origBlockID, warming, grazing) %>% 
+  bind_cols(nitrogen)
+
+# Low (is done separately because it is only destination site)
+low <- tibble(
+  origSiteID = factor("Low", levels = c("Top", "Middle", "Low")),
+  origBlockID = rep(1:10, each = 4),
+  origPlotID = 161:200,
+  #destSiteID = factor(NA, levels = c("Top", "Middle", "Low")),
+  Nlevel = rep(c(4,1,2,3,6,5,10,9,7,8), each = 4),
+  warming = "W",
+  grazing = rep(c("notN", "notN", "notN", "N"), 10),
+  fence = if_else(grazing == "N", "out", "in"))
+
+# randomize warming and grazing treatment
+set.seed(33) # seed is needed to replicate sample_frac
+set.seed(seed = 33, sample.kind = "Rounding")
+meta2 <- meta %>% 
+  # create variable for grazing treatment inside or outside fence
+  mutate(fence = if_else(grazing == "N", "out", "in")) %>% 
+  mutate(origSiteID = factor(origSiteID, levels = c("Top", "Middle", "Low"))) %>%
+  arrange(origSiteID) %>% # site needs to be arranged, because transplant goes only in one direction
+  group_by(origSiteID, origBlockID, Nlevel, fence) %>%
+  sample_frac() %>% # randomization
+  ungroup() %>% 
+  bind_cols(origPlotID) %>% # add plotID
+  mutate(destSiteID = case_when(
+    origSiteID == "Top" & warming == "A" ~ "Top",
+    origSiteID == "Middle" & warming == "W" ~ "Low",
+    TRUE ~ "Midlle")) %>%
+  #mutate(destSiteID = factor(destSiteID, levels = c("Top", "Middle", "Low"))) %>%
+  bind_rows(low) %>% # add low
+  group_by(origSiteID, origBlockID, warming, fence) %>% 
+  mutate(rownr = row_number())
+
+
+# Join meta2 to warmed plots
+metaTurfID_China <- left_join(
+  meta2 %>% filter(origPlotID < 161), # remove plots from vik
+  # only warmed plots, remove unused rows
+  meta2 %>% filter(warming == "W") %>% select(-grazing, -destSiteID, destPlotID = origPlotID), 
+  by = c("destSiteID" = "origSiteID", "origBlockID" = "origBlockID", "rownr" = "rownr", "fence" = "fence", "Nlevel" = "Nlevel", "warming" = "warming"), 
+  suffix = c("", "_dest")) %>% 
+  mutate(destBlockID = origBlockID,
+         destPlotID = ifelse(is.na(destPlotID), origPlotID, destPlotID),
+         turfID = paste0(origPlotID, " ", warming, "N", Nlevel, grazing,  " ", destPlotID)) %>% 
+  ungroup() %>% 
+  select(-fence, -rownr)
