e17600dd69513c4517916133e3537c58ea5c75cc
audhalbritter <me@audhalbritter.com>
2021-01-06 08:42:51 +0100

    fix biomass cleaning code

--- R/Vegetation/ReadInBiomass.R
+++ R/Vegetation/ReadInBiomass.R
@@ -3,44 +3,41 @@
 ############################
 
 source("R/Load packages.R")
+source("R/Rgathering/create meta data.R")
 
-read_excel(path = "data/biomass/THREE_D_Biomass_Cutting_2019_Leire.xlsx")
-
-
-files <- dir(path = "data/biomass/", pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
-files <- files[c(1, 3)]
-
-map_df(set_names(files), function(file) {
-  file %>% 
-    set_names() %>% 
-    map_df(~ read_excel(path = file))
-}, .id = "File")
-
-# , col_types = c("text", "numeric", "numeric", "text", "text", "numeric", "text", "numeric", "date", rep("numeric", 6), "text", "numeric", "numeric")
-
-biomass_raw1 <- read_excel(path = "data/biomass/THREE_D_Biomass_Cutting_2019_Leire.xlsx", col_types = c("text", "numeric", "numeric", "text", "text", "numeric", "text", "numeric", "date", rep("numeric", 6), "text", "numeric", "numeric"))
-biomass_raw2 <- read_excel(path = "data/biomass/THREE_D_Biomass_Cutting_2019_Selina.xlsx", col_types = c("text", "numeric", "numeric", "text", "text", "numeric", "text", "numeric", "date", rep("numeric", 6), "text", "numeric", "numeric", "numeric", "numeric"))
-
-biomass <- biomass_raw1 %>%
-  bind_rows(biomass_raw2) %>% 
-  mutate(Bryophytes_g = coalesce(Bryophytes_g, Moss),
-         Cyperaceae_g = coalesce(Cyperaceae_g, carex)) %>% 
-  select(destSiteID:Date, Graminoids_g:Litter_g, Lichen_g, Shrub_g, Remark, Moss) %>% 
-  pivot_longer(cols = c(Graminoids_g:Shrub_g), names_to = "FunGroup", values_to = "Value") %>% 
+biomass_raw <- read_excel(path = "data/biomass/THREE_D_Biomass_Grazing_2020.xlsx", col_types = c("text", "numeric", "numeric", "text", "text", "numeric", "text", "numeric", "date", rep("numeric", 6), "text", rep("numeric", 4))) %>% 
+  pivot_longer(cols = c(Graminoids_g:Litter_g, Lichen_g:Fungi_g), names_to = "fun_group", values_to = "value") %>% 
   filter(grazing %in% c("M", "I"),
-         !is.na(Value)) %>% 
-  distinct()
+         !is.na(value)) %>% 
+  rename(date = Date, cut = Cut, remark = Remark) %>% 
+  left_join(metaTurfID, by = c("destSiteID", "destBlockID", "destPlotID", "turfID", "warming", "Nlevel", "grazing"))
 
-biomass %>% filter(turfID == "14 WN6I 92") %>% as.data.frame()
- # no date
-biomass %>% filter(is.na(Date)) %>% distinct(turfID, Cut)
+# get unique cutting dates
+cutting_date <- biomass_raw %>% 
+  mutate(year = year(date)) %>% 
+  group_by(year, destSiteID, turfID, grazing, cut) %>% 
+  distinct(date) %>% 
+  rename(cutting_date = date)
 
-incomplete <- biomass %>% 
-  group_by(destSiteID, turfID, FunGroup) %>% 
-  mutate(n = n()) %>% 
-  # all incomplete once
-  filter((grazing == "M" & n < 2) | (grazing == "I" & n < 4)) %>% 
-  select(Date, destSiteID, turfID, grazing, Cut, FunGroup, Value, Remark, n) %>% 
-  arrange(destSiteID, turfID)
 
-write_csv(incomplete, path = "Incomplete_Biomass_ThreeD_2020.csv")
+# sum biomass up per plot
+biomass <- biomass_raw %>% 
+  mutate(year = year(date)) %>% 
+  group_by(turfID, year, fun_group, origSiteID, warming, grazing, Nlevel) %>% 
+  summarise(sum = mean(value, na.rm = TRUE))
+
+
+
+# check data
+biomass_raw %>% 
+  filter(cut == 1) %>% 
+  ggplot(aes(x = as.factor(Nlevel), y = value, fill = warming)) +
+  geom_boxplot() +
+  facet_grid(origSiteID ~ fun_group)
+
+
+biomass %>% 
+  filter(fun_group %in% c("Graminoids_g", "Forbs_g", "Legumes_g")) %>% 
+  ggplot(aes(x = as.factor(Nlevel), y = sum, colour = warming, shape = grazing)) +
+  geom_point() +
+  facet_grid(fun_group ~ origSiteID, scales = "free_y")
