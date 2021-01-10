89ca499b41e6176c25202271a92a1efc213d0615
audhalbritter <me@audhalbritter.com>
2021-01-10 21:54:08 +0100

    download files

--- R/Rgathering/DownloadCleanData.R
+++ R/Rgathering/DownloadCleanData.R
@@ -4,18 +4,52 @@
 #devtools::install_github("Between-the-Fjords/dataDownloader")
 library("dataDownloader")
 
-#Download community data from OSF
+### VEGETATION
 get_file(node = "pk4bg",
          file = "THREE-D_CommunitySubplot_2019.csv",
-         path = "data_cleaned/",
-         remote_path = "Community")
+         path = "data_cleaned/vegetation",
+         remote_path = "Vegetation")
 
 get_file(node = "pk4bg",
          file = "THREE-D_Cover_2019.csv",
-         path = "data_cleaned/",
-         remote_path = "Community")
+         path = "data_cleaned/vegetation",
+         remote_path = "Vegetation")
 
 get_file(node = "pk4bg",
          file = "THREE-D_CommunityStructure_2019.csv",
-         path = "data_cleaned/",
-         remote_path = "Community")
+         path = "data_cleaned/vegetation",
+         remote_path = "Vegetation")
+
+get_file(node = "pk4bg",
+         file = "THREE-D_Reflectance_2020.csv",
+         path = "data_cleaned/vegetation",
+         remote_path = "Vegetation")
+
+### SOIL
+get_file(node = "pk4bg",
+         file = "THREE-D_PlotLevel_Depth_2019.csv",
+         path = "data_cleaned/soil",
+         remote_path = "Soil")
+
+get_file(node = "pk4bg",
+         file = "THREE-D_Soil_2019-2020.csv",
+         path = "data_cleaned/soil",
+         remote_path = "Soil")
+
+
+### C-FLUX
+get_file(node = "pk4bg",
+         file = "Three-D_c-flux_2020.csv",
+         path = "data_cleaned/c-flux",
+         remote_path = "C-Flux")
+
+### CLIMATE
+get_file(node = "pk4bg",
+         file = "THREE_D_Gridded_DailyClimate_2009-2019.csv",
+         path = "data_cleaned/climate",
+         remote_path = "Climate")
+
+get_file(node = "pk4bg",
+         file = "THREE-D_TomstLogger_2019_2020.csv",
+         path = "data_cleaned/climate",
+         remote_path = "Climate")
