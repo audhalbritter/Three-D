2d8fc912d899242477a6acab9a721028d34d4b78
audhalbritter <me@audhalbritter.com>
2021-11-22 10:40:36 +0100

    2021 biomass data

--- R/Vegetation/ReadInBiomass_grazing.R
+++ R/Vegetation/ReadInBiomass_grazing.R
@@ -12,7 +12,7 @@ source("R/Rgathering/create meta data.R")
 #          remote_path = "RawData/Vegetation")
 
 # 2020 data
-biomass <- read_excel(path = "data/biomass/THREE_D_Biomass_Grazing_2020_March_2021.xlsx", 
+biomass20 <- read_excel(path = "data/biomass/THREE_D_Biomass_Grazing_2020_March_2021.xlsx", 
                           col_types = c("text", "numeric", "numeric", "text", "text", "numeric", "text", "numeric", "date", rep("numeric", 6), "text", rep("numeric", 4))) %>% 
   pivot_longer(cols = c(Graminoids_g:Litter_g, Lichen_g:Fungi_g), names_to = "fun_group", values_to = "value") %>% 
   filter(grazing %in% c("M", "I"),
@@ -21,15 +21,38 @@ biomass <- read_excel(path = "data/biomass/THREE_D_Biomass_Grazing_2020_March_20
   left_join(metaTurfID, by = c("destSiteID", "destBlockID", "destPlotID", "turfID", "warming", "Nlevel", "grazing")) %>% 
   mutate(year = year(date))
 
-write_csv(biomass, path = "data_cleaned/vegetation/THREE-D_Biomass_2020.csv")
+#write_csv(biomass, path = "data_cleaned/vegetation/THREE-D_Biomass_2020.csv")
 
 # 2021 data
-# FIX MISSING DATA IN CUT 1
-biomass21 <- read_excel(path = "data/biomass/THREE_D_Biomass_2021_09_29.xlsx",
+biomass21_raw <- read_excel(path = "data/biomass/THREE_D_Raw_Biomass_2021_10_21.xlsx",
            col_types = c("text", "numeric", "text", "text", "numeric", "numeric", "text", "numeric", "numeric", "text", "numeric", "date", rep("numeric", 9), "text", "text", rep("numeric", 6))) %>% 
   pivot_longer(cols = c(Graminoids_g:Fungi_g), names_to = "fun_group", values_to = "value") %>% 
   filter(!is.na(value)) %>% 
-  mutate(year = 2021) # make automatic from date when data is fixed
+  rename(date = Date, cut = Cut, remark = Remark, collector = Collector) %>% 
+  mutate(year = year(date)) %>% 
+  #### REMOVE CUT 2 BECAUSE THERE IS SOME PROBLEM WITH THE DATA!!!
+  filter(cut != 2)
+
+# biomass for L shaped plots
+dd <- biomass21_raw %>% 
+  filter(!is.na(top)) %>% 
+  # outer square minus inner square
+  mutate(area_L = top * r_side - inner_l_side * inner_top)
+
+
+
+biomass %>% 
+  filter(fun_group %in% c("Graminoids_g", "Forbs_g")) %>% 
+  group_by(turfID, grazing) %>% 
+  count(cut) %>% View()
+
+
+
+biomass21 %>% distinct(fun_group)
+biomass21 %>% View()
+biomass21 %>% filter(Cut == 3, origSiteID == "Joa") %>% distinct(grazing, Date) %>% arrange(grazing)
+
+# find cutting date for cut 4 turfID 110 AN3I 110
 
 # get unique cutting dates
 cutting_date <- biomass %>% 
