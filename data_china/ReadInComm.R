fff4122149a67e0da5a8b17526e9375449c793c4
audhalbritter <me@audhalbritter.com>
2021-12-14 12:51:22 +0100

    update china data corrections

--- data_china/ReadInComm.R
+++ data_china/ReadInComm.R
@@ -6,27 +6,14 @@ source("R/Rgathering/create meta data.R")
 
 # turfID dic for wrong turfIDs in 2021
 
-# fix site names in meta data
-meta_c <- metaTurfID_China %>% 
+# # fix site names in meta data
+meta_c <- metaTurfID_China %>%
   mutate(origSiteID = recode(origSiteID,  "Top" = "H", "Middle" = "M", "Low" = "L"),
          destSiteID = recode(destSiteID,  "Top" = "H", "Middle" = "M", "Low" = "L"))
 
-turfID_2021_dic <- bind_cols(
-  meta_c %>% 
-    filter(origSiteID == "H", warming == "W") %>% 
-    select(turfID) %>% 
-    rename(correct_turfID = turfID),
-  meta_c %>% 
-    filter(origSiteID == "M", warming == "W") %>% 
-    select(turfID) %>% 
-    rename(wrong_turfID = turfID)
-) %>% 
-  mutate(destSiteID = "M")
-
-write_csv(turfID_2021_dic, file = "data_china/turfID_2021_dic.csv")
 
 # read in 2019 - 2021 data
-files <- dir(path = "data_china/Data-Community/", pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
+files <- dir(path = "data_china/", pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
 
 #Function to read in meta data
 metaComm_raw <- map_df(set_names(files), function(file) {
@@ -41,17 +28,15 @@ metaComm_raw <- map_df(set_names(files), function(file) {
 }, .id = "file")
 
 
-metaComm_raw2 <- metaComm_raw %>% 
+metaComm <- metaComm_raw %>% 
   select(file, sheet_name, Date, destSiteID, destBlockID, destPlotID, turfID, Recorder, Scribe) %>% 
-  mutate(Year = year(Date))
-
-metaComm <- metaComm_raw2 %>% 
+  mutate(Year = year(Date)) %>% 
   filter(Year == 2019) %>% 
   # change dest to orig, because before transplant
   rename(origSiteID = destSiteID, origBlockID = destBlockID, origPlotID = destPlotID) %>% 
   mutate(turfID = gsub("_", " ", turfID),
          turfID = gsub("  ", " ", turfID),
-         # fix wrong blockID
+         # fix wrong plotID, blockID and siteID
          origBlockID = case_when(turfID == "54 WN4C 134" ~ 7,
                                  turfID == "65 WN9I 146" ~ 9,
                                  turfID == "66 AN9M 66" ~ 9,
@@ -64,51 +49,31 @@ metaComm <- metaComm_raw2 %>%
                                  turfID == "80 AN2N 80" ~ 10,
                                  TRUE ~ origBlockID),
          # fix wrong plotID
-         origPlotID = if_else(turfID == "121 AN7M 121", 121, origPlotID)) %>% 
+         origPlotID = if_else(turfID == "121 AN7M 121", 121, origPlotID),
+         # siteID
+         origSiteID = if_else(origSiteID == "L", "M", origSiteID),
+         origSiteID = if_else(str_detect(file, "H-community") & origSiteID == "M", "H", origSiteID)) %>% 
+  # join meta data
   left_join(meta_c, by = c("origSiteID", "origBlockID", "origPlotID", "turfID")) %>% 
 
   # bind rows with 2021 data
   bind_rows(
     metaComm_raw2 %>% 
       filter(Year == 2021) %>% 
-      mutate(destSiteID = as.character(destSiteID),
-             # recode site names to match 2019 data
-             destSiteID = recode(destSiteID,  "4400" = "H", "4000" = "M", "3500" = "L")) %>% 
-    # fix wrong turfIDs
-    left_join(turfID_2021_dic, by = c("turfID" = "wrong_turfID", "destSiteID")) %>% 
-    mutate(turfID = if_else(!is.na(correct_turfID), correct_turfID, turfID)) %>% 
-    select(-correct_turfID) %>% 
-    # fix wrong turfID, destplotID and destBlockID
-    mutate(turfID = if_else(turfID == "9 WN6I 89" & destSiteID == "H", "11 AN6M 11", turfID),
+      mutate(turfID = if_else(turfID == "9 WN6I 89" & destSiteID == "H", "11 AN6M 11", turfID),
            destPlotID = case_when(turfID == "101 WN5M 171" ~ 171,
-                                  turfID == "79 WN2N 159" ~ 159,
-                                  turfID == "160 AN2N 160" ~ 160,
+                                  #turfID == "79 WN2N 159" ~ 159,
+                                  #turfID == "160 AN2N 160" ~ 160,
                                   turfID == "74 AN2C 74" ~ 74,
                                   TRUE ~ destPlotID),
-           destBlockID = case_when(turfID == "79 WN2N 159" ~ 10,
-                                   turfID == "160 AN2N 160" ~ 10,
-                                   turfID == "1 AN1I 1" ~ 1,
-                                   turfID == "3 AN1C 3" ~ 1,
-                                   turfID == "5 AN1M 5" ~ 1,
-                                   turfID == "7 AN1N 7" ~ 1,
-                                   turfID == "11 AN6M 11" ~ 2,
-                                   turfID == "12 AN6I 12" ~ 2,
-                                   turfID == "14 AN6C 14" ~ 2,
-                                   turfID == "16 AN6N 16" ~ 2,
-                                  TRUE ~ destBlockID)) %>% 
-    left_join(meta_c, by = c("destSiteID", "destBlockID", "destPlotID", "turfID"))
-    )
-
-  
-# no data in 2021, are they missing?
-# 159 WN2N 200
-# 160 AN2N 160
+           destSiteID = if_else(turfID %in% c("34 AN10M 34", "36 AN10C 36", "38 AN10I 38", "39 AN10N 39"), "H", destSiteID)) %>% 
+    left_join(meta_c, by = c("destSiteID", "destBlockID", "destPlotID", "turfID")))
 
 
 # Read in community data
-files <- dir(path = "data_china/Data-Community", pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
+files <- dir(path = "data_china/", pattern = "\\.xlsx$", full.names = TRUE, recursive = TRUE)
 
-comm <- map_df(set_names(files), function(file) {
+comm_raw <- map_df(set_names(files), function(file) {
   file %>% 
     excel_sheets() %>% 
     set_names() %>% 
@@ -117,7 +82,7 @@ comm <- map_df(set_names(files), function(file) {
 }, .id = "file") %>% 
   rename("Cover" = `%`) 
 
-comm2 <- comm %>% 
+comm <- comm_raw %>% 
   # remove empty rows
   filter(!is.na(Species)) %>% 
   # extract year from file path, first 4 digit
@@ -125,40 +90,111 @@ comm2 <- comm %>%
          # remove underscore from sheet name
          sheet_name = gsub("_", " ", sheet_name),
          # extract site from file name and rename
-         site = dirname(file) %>% str_remove("^.*Site_") %>% recode(., "4400m" = "H", "4000m" = "M", "3500m" = "L"),
+         site = basename(file) %>% str_sub(., 1, 1),
          # 2019 is origin site, 2021 is dest site
          origSiteID = if_else(Year == 2019, site, NA_character_),
-         destSiteID = if_else(Year == 2021, site, NA_character_)) %>% 
-  select(-site) %>% 
-  # fix wrong turfIDs in 2021
-  left_join(turfID_2021_dic, by = c("sheet_name" = "wrong_turfID", "destSiteID")) %>% 
-  mutate(sheet_name = if_else(!is.na(correct_turfID), correct_turfID, sheet_name)) %>% 
-  select(-correct_turfID)
+         destSiteID = if_else(Year == 2021, site, NA_character_),
+         sheet_name = if_else(sheet_name == "98 WN5I 169", "98 AN5C 98", sheet_name),
+         sheet_name = if_else(sheet_name == "118  AN10C 118", "118 AN10C 118", sheet_name)
+         ) %>% 
+  select(-site)
 
-duplicates <- metaComm %>% 
-  rename(sheet_name2 = sheet_name) %>% 
-  left_join(comm2 %>% select(-file), by = c("turfID" = "sheet_name", "Year")) %>% 
+
+
+community <- metaComm %>% 
+  # should be empty if all match
+  #anti_join(comm %>% select(-file), by = c("turfID" = "sheet_name", "Year")) %>% View()
+  left_join(comm %>% select(-file), by = c("turfID" = "sheet_name", "Year")) %>% 
   mutate(origSiteID = coalesce(origSiteID.x, origSiteID.y),
          destSiteID = coalesce(destSiteID.x, destSiteID.y)) %>% 
   select(-origSiteID.x, -origSiteID.y, -destSiteID.x, -destSiteID.y) %>% 
-  rename(date = Date, year = Year, species = Species, cover = Cover, recorder = Recorder, scribe = Scribe, remark = Remark) %>% 
+  rename(year = Year, date = Date, species = Species, cover = Cover, recorder = Recorder, scribe = Scribe, remark = Remark) %>% 
   select(origSiteID, origBlockID, origPlotID, destSiteID, destPlotID, destBlockID, turfID, warming, grazing, Nlevel, year, date, species, cover, "1":"25", recorder, scribe, remark, file) %>% 
-  group_by(turfID, year, species) %>% 
   
-  # Need to fix duplicates!!!
-  #group_by(origSiteID, origBlockID, origPlotID, destSiteID, destPlotID, destBlockID, turfID, warming, grazing, Nlevel, year, species, cover, recorder, scribe, remark, file) %>% 
-  mutate(n = n()) %>% filter(n > 1)
-  # remove 2 rows with same cover
-  #slice(1)
-
-write_csv(duplicates, file = "data_china/duplicates.csv")
-
-### 2 datasheets are called the same! 2021, H, bl 10: 4 AN2C 74
+  # check for duplicates
+  # group_by(origSiteID, origBlockID, origPlotID, destSiteID, destPlotID, destBlockID, turfID, warming, grazing, Nlevel, year, species, cover, recorder, scribe, remark, file) %>%
+  # mutate(n = n()) %>% filter(n > 1)
   
-# problems that need fixing:
-# duplicate species per plot, some with same cover, some have not the same cover
-# species was missing in one sheet
-# check sheet name and turfIDs, site, plot and block names
-# turfID and sheet name is wrong in 2021 for plots that were moved from H to M.
+  # remove damaged plots in 2021 (no data)
+  # 103 AN5N 103, 112 AN3N 112, 135 AN4N 135, 64 WN8N 143, 79 WN2N 159, 160 AN2N 160
+  tidylog::filter(!species %in% c("Be destroyed", "样方未找到")) %>% 
+  
+  # remove empty rows
+  filter(!species %in% c("Fertile (bud, flower, seed, fruit)", "Presence", "Subplot recording (highest level):")) %>% 
+  
+  # fix species names
+  mutate(species = case_when(str_detect(species, "Anaphalis nepalensis") ~ "Anaphalis nepalensis",
+                             species == "Caltha palustris Linnaeus" ~ "Caltha palustris",
+                             species == "Carex atrofusca subsp.minor" ~ "Carex atrofusca subsp. minor",
+                             species == "Crambe Linn" ~ "Crambe sp.",
+                             species == "Cyperus rotundus L." ~ "Cyperus rotundus",
+                             species == "Fragaria orientalis L" ~ "Fragaria orientalis",
+                             species == "Galium spurium L" ~ "Galium spurium",
+                             species == "Juncus allioides Franchet" ~ "Juncus allioides",
+                             species == "oxytropis kansuensis" ~ "Oxytropis kansuensis",
+                             species == "Pedicularis sima M" ~ "Pedicularis sima",
+                             species == "Poa pratensis Linnaeus" ~ "Poa pratensis",
+                             species == "Polygonum viviparum Linnaeus" ~ "Polygonum viviparum",
+                             species == "Potentillaleuconota" ~ "Potentilla leuconota",
+                             species == "Rhodiola" ~ "Rhodiola sp.",
+                             species == "Rhodiola rosea Linn." ~ "Rhodiola rosea",
+                             species == "Saussurea graminea Dunn" ~ "Saussurea graminea",
+                             species == "Saussurea pachyneura Franch." ~ "Saussurea pachyneura",
+                             species == "Stellaria decumbensvar. pulvinata" ~ "Stellaria decumbens var. pulvinata",
+                             TRUE ~ species)) %>% 
+  
+  # fix cover
+  mutate(cover = if_else(cover %in% c("<0.5", "<0.2", "《0.5"), "0.1", cover),
+         cover = as.numeric(cover)) %>% 
+  
+  # fix cover in remark column
+  mutate(cover = case_when(remark == "1" ~ 1,
+                           remark == "10" ~ 10,
+                           remark == "3" ~ 3,
+                           remark == "5" ~ 5,
+                           TRUE ~ cover),
+         remark = if_else(remark %in% c("1", "10", "3", "5"), NA_character_, remark)) %>% 
+          
+  # add weather from scribe column
+  mutate(weather = if_else(scribe %in% c("sunny", "cloudy", "Rain", "Sun"), scribe, NA_character_))
+
+# needs fixing
+community %>% filter(species == "Geranium pylzowianumAletris pauciflora")
+# Geranium pylzowianumAletris pauciflora
+
+# Cover data
+cover <- community %>% 
+  filter(!species %in% c("Bare rock", "Bare soil", "Bryophyes", "Lichen", "Litter", "Poop", "Vascular plants", "Total Cover (%)", "Height / depth (cm)", "Vascular plant layer", "Moss layer")) %>% 
+  select(year, date, origSiteID:Nlevel, species, cover, weather, recorder, file)
+write_csv(x = cover, file = "data_china/China_clean_cover_2019_2021.csv")
+
+# Subplot community data
+subplot_community <- community %>% 
+  filter(!species %in% c("Bare rock", "Bare soil", "Bryophyes", "Lichen", "Litter", "Poop", "Vascular plants", "Total Cover (%)", "Height / depth (cm)", "Vascular plant layer", "Moss layer")) %>% 
+  pivot_longer(cols = c("1":"25"), names_to = "subplot", values_to = "presence") %>% 
+  filter(!is.na(presence)) %>% 
+  mutate(fertile = if_else(presence == "F", "F", NA_character_),
+         dominant = if_else(presence == "D", "D", NA_character_),
+         seedling = if_else(presence == "S", "S", NA_character_),
+         presence = if_else(!is.na(presence), "1", NA_character_)) %>% 
+  pivot_longer(cols = c(fertile, dominant, seedling, presence), names_to = "variable", values_to = "value") %>% 
+  filter(!is.na(value)) %>% 
+  select(year, date, origSiteID:Nlevel, subplot, species, variable, value, weather, recorder, file)
+write_csv(x = subplot_community, file = "data_china/China_clean_subplot_community_2019_2021.csv")
+
+
+# functional group cover
+community %>% 
+  filter(species %in% c("Bare rock", "Bare soil", "Bryophyes", "Lichen", "Litter", "Poop", "Vascular plants", "Total Cover (%)")) %>% 
+  filter(!is.na(cover))
+
+                             
+# height
+community %>% 
+  filter(species %in% c("Height / depth (cm)", "Vascular plant layer", "Moss layer")) %>% 
+  rename(h1 = "1", h2 = "2", h3 = "3", h4 = "4", h5 = "5") %>% 
+  filter(!is.na(h2))
+
+
 
   
