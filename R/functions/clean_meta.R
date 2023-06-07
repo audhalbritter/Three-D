1f01ef02f3453f6de2a16b49550ee6a5a7065d8a
audhalbritter <me@audhalbritter.com>
2023-06-07 11:38:33 +0200

    add meta data claening

---
+++ R/functions/clean_meta.R
@@ -0,0 +1,28 @@
+# clean meta data
+
+clean_plot <- function(plot_raw, metaTurfID){
+  
+  #### PLOT LEVEL META DATA ####
+  plot_clean <- plot_raw %>% 
+    # remove rows with data after transplant, duplicate
+    filter(remark != "after transplant" | is.na(remark)) %>% 
+    left_join(metaTurfID, by = c("origSiteID", "origBlockID", "origPlotID", "destSiteID", "destPlotID", "destBlockID", "turfID")) %>% 
+    # calculate mean soil depth
+    mutate(soil_depth_cm = (soil_depth1 + soil_depth2 + soil_depth3 + soil_depth4) / 4,
+           soil_depth_cm = if_else(origPlotID == 106, 36.1, soil_depth_cm),
+           year = 2019) %>% 
+    select(year, origSiteID:turfID, warming:Nlevel, Namount_kg_ha_y, date_slope:slope, aspect = exposure, date_depth, soil_depth_cm, remark)
+  
+  plot_clean
+  
+}
+
+
+
+# save data as csv
+save_csv <- function(file, name) {
+  
+  filepath <- paste0("data_cleaned/", "Three-D_", name, ".csv")
+  output <- write_csv(x = file, file = filepath)
+  filepath
+}
---
+++ R/meta_plan.R
@@ -0,0 +1,58 @@
+# meta data plan
+
+meta_plan <- list(
+  
+  # download site data
+  tar_target(
+    name = site_download,
+    command = get_file(node = "pk4bg",
+                       file = "Three-D_meta_site.csv",
+                       path = "data",
+                       remote_path = "Site"),
+    format = "file"
+  ),
+  
+  # import site data
+  tar_target(
+    name = site,
+    command = read_csv(site_download)
+  ),
+  
+  # download plot data
+  tar_target(
+    name = plot_download,
+    command = get_file(node = "pk4bg",
+                       file = "Three-D_PlotLevel_MetaData_2019.csv",
+                       path = "data",
+                       remote_path = "RawData/Soil"),
+    format = "file"
+  ),
+  
+  # import plot data
+  tar_target(
+    name = plot_raw,
+    command = read_csv(plot_download)
+  ),
+  
+  # clean plot data
+  tar_target(
+    name = plot_clean,
+    command = clean_plot(plot_raw, metaTurfID)
+  ),
+  
+  # save clean plot data
+  tar_target(
+    name = plot_out,
+    command = save_csv(plot_clean, 
+                       name = "soil_depth_2019")
+  ),
+  
+  # meta turfID
+  tar_target(
+    name = metaTurfID,
+    command = create_threed_meta_data()
+  )
+  
+  
+  
+)
