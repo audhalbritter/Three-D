c6d95644fbbca56bab395c295b780fa32a1eeb28
audhalbritter <me@audhalbritter.com>
2023-09-28 13:10:00 +0200

    add new datasets to data dic plan

--- R/data_dic_plan.R
+++ R/data_dic_plan.R
@@ -38,6 +38,62 @@ data_dic_plan <- list(
                                    table_ID = NA_character_)
   ),
   
+  # biomass data dic
+  tar_target(
+    name = biomass_dic,
+    command = make_data_dictionary(data = biomass_clean,
+                                   description_table = attribute_table,
+                                   table_ID = NA_character_)
+  ),
+  
+  # cover data dic
+  tar_target(
+    name = cover_dic,
+    command = make_data_dictionary(data = cover_clean,
+                                   description_table = attribute_table,
+                                   table_ID = NA_character_)
+  ),
+  
+  # subplot presence data dic
+  tar_target(
+    name = presence_dic,
+    command = make_data_dictionary(data = subplot_presence_clean,
+                                   description_table = attribute_table,
+                                   table_ID = NA_character_)
+  ),
+  
+  # community structure data dic
+  tar_target(
+    name = comm_structure_dic,
+    command = make_data_dictionary(data = comm_structure_clean,
+                                   description_table = attribute_table,
+                                   table_ID = NA_character_)
+  ),
+  
+  # community structure data dic
+  tar_target(
+    name = height_dic,
+    command = make_data_dictionary(data = height_clean,
+                                   description_table = attribute_table,
+                                   table_ID = NA_character_)
+  ),
+  
+  # reflectance data dic
+  tar_target(
+    name = ndvi_dic,
+    command = make_data_dictionary(data = ndvi_clean,
+                                   description_table = attribute_table,
+                                   table_ID = NA_character_)
+  ),
+  
+  # root data dic
+  tar_target(
+    name = root_dic,
+    command = make_data_dictionary(data = roots_clean,
+                                   description_table = attribute_table,
+                                   table_ID = NA_character_)
+  ),
+  
   
   
   # merge data dictionaries
@@ -45,12 +101,13 @@ data_dic_plan <- list(
     name = threeD_dic,
     command = write_xlsx(list(site = site_dic,
                               plot = plot_dic,
-                              climate = climate_dic#,
-                                # community_cover = cover_dic,
-                                # subplot_presence = subplot_dic,
-                                # community_structure = structure_dic,
-                                # biomass = biomass_dic,
-                                # ndvi = reflectance_dic,
+                              climate = climate_dic,
+                              biomass = biomass_dic,
+                              comm_cover = cover_dic,
+                              subplot_presence_dic = presence_dic,
+                              comm_structure = comm_structure_dic,
+                              height = height_dic,
+                              ndvi = ndvi_dic#,
                                 # soil= soil_dic,
                                 # soil_nutrients = prs_dic,
                                 # decomposition = decompose_dic,
