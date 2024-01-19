fdeb12ad694eabe147e33c972cbedf1b5ede8992
audhalbritter <me@audhalbritter.com>
2024-01-19 17:13:38 +0100

    change site names

--- R/functions/clean_reflectance.R
+++ R/functions/clean_reflectance.R
@@ -4,19 +4,37 @@ clean_reflectance <- function(ndvi20_raw, ndvi20_2_raw, ndvi22_raw, metaTurfID){
   
   # 2020 data
   ndvi20 <- ndvi20_raw %>% 
+    # change site names
+    mutate(origSiteID = case_when(origSiteID == "Joa" ~ "Joasete",
+                                  origSiteID == "Lia" ~ "Liahovden",
+                                  TRUE ~ origSiteID),
+           destSiteID = case_when(destSiteID == "Joa" ~ "Joasete",
+                                  destSiteID == "Lia" ~ "Liahovden",
+                                  destSiteID == "Vik" ~ "Vikesland",
+                                  TRUE ~ destSiteID)) |> 
     rename(nr = measurement) %>% 
     mutate(date = dmy(date),
            year = year(date)) |> 
     left_join(metaTurfID)
   
   ndvi20_2 <- ndvi20_2_raw %>% 
+    # change site names
+    mutate(destSiteID = case_when(destSiteID == "Joa" ~ "Joasete",
+                                  destSiteID == "Lia" ~ "Liahovden",
+                                  destSiteID == "Vik" ~ "Vikesland",
+                                  TRUE ~ destSiteID)) |> 
     mutate(date = dmy(date),
            year = year(date)) %>% 
     select(year, date, destSiteID:turfID, nr, campaign, ndvi) |> 
     left_join(metaTurfID)
     
   # 2022 data
-  ndvi22 <- ndvi22_raw %>% 
+  ndvi22 <- ndvi22_raw |> 
+    # change site names
+    mutate(destSiteID = case_when(destSiteID == "Joa" ~ "Joasete",
+                                  destSiteID == "Lia" ~ "Liahovden",
+                                  destSiteID == "Vik" ~ "Vikesland",
+                                  TRUE ~ destSiteID)) |> 
     filter(!is.na(ndvi)) |> 
     mutate(year = year(date)) |> 
     left_join(metaTurfID)
@@ -27,17 +45,17 @@ clean_reflectance <- function(ndvi20_raw, ndvi20_2_raw, ndvi22_raw, metaTurfID){
     # remove empty rows
     filter(!is.na(ndvi)) %>% 
     # convert campaign nr to timing
-    mutate(timing = case_when(destSiteID == "Lia" & campaign == 2 ~ "After 1. cut",
-                              destSiteID == "Lia" & campaign == 3 ~ "After 1. cut",
-                              destSiteID == "Lia" & campaign == 4 ~ "After 2. cut",
+    mutate(timing = case_when(destSiteID == "Liahovden" & campaign == 2 ~ "After 1. cut",
+                              destSiteID == "Liahovden" & campaign == 3 ~ "After 1. cut",
+                              destSiteID == "Liahovden" & campaign == 4 ~ "After 2. cut",
                               
-                              destSiteID == "Joa" & campaign == 2 ~ "After 1. cut",
-                              destSiteID == "Joa" & campaign == 3 ~ "After 2. cut",
-                              destSiteID == "Joa" & campaign == 4 ~ "After 3. cut",
+                              destSiteID == "Joasete" & campaign == 2 ~ "After 1. cut",
+                              destSiteID == "Joasete" & campaign == 3 ~ "After 2. cut",
+                              destSiteID == "Joasete" & campaign == 4 ~ "After 3. cut",
                               
-                              destSiteID == "Vik" & campaign == 2 ~ "After 2. cut",
-                              destSiteID == "Vik" & campaign == 3 ~ "After 2. cut",
-                              destSiteID == "Vik" & campaign == 4 ~ "After 3. cut",
+                              destSiteID == "Vikesland" & campaign == 2 ~ "After 2. cut",
+                              destSiteID == "Vikesland" & campaign == 3 ~ "After 2. cut",
+                              destSiteID == "Vikesland" & campaign == 4 ~ "After 3. cut",
                               
                               year == 2022 ~ "after 2. cut",
                               TRUE ~ timing)) |> 
