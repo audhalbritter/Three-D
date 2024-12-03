90e7aee58702570f7133c3cee23435594d01054a
jogaudard <joseph.gaudard@pm.me>
2024-12-03 15:21:17 +0100

    clean cflux file

--- R/cflux_plan.R
+++ R/cflux_plan.R
@@ -9,4 +9,36 @@ cflux_plan <- list(
                        remote_path = "RawData/C-Flux"),
     format = "file"
   ),
+  tar_target(
+    name = cfluxrecord2020_download,
+    command = get_file(node = "pk4bg",
+                       file = "Three-D_field-record_2020.csv",
+                       path = "data/C-Flux/summer_2020",
+                       remote_path = "RawData/C-Flux"),
+    format = "file"
+  ),
+  tar_target(
+    name = cflux2021_download,
+    command = get_file(node = "pk4bg",
+                       file = "Three-D_cflux_2021.zip",
+                       path = "data/c-flux/summer_2021",
+                       remote_path = "RawData/C-Flux"),
+    format = "file"
+  ),
+  tar_target(
+    name = cfluxrecord2021_download,
+    command = get_file(node = "pk4bg",
+                       file = "Three-D_field-record_2021.csv",
+                       path = "data/c-flux/summer_2021",
+                       remote_path = "RawData/C-Flux"),
+    format = "file"
+  ),
+  tar_target(
+    name = soilRchambersize_download,
+    command = get_file(node = "pk4bg",
+                       file = "Three-D_soilR-chambers-size.csv",
+                       path = "data/c-flux/summer_2021",
+                       remote_path = "RawData/C-Flux"),
+    format = "file"
+  ),
 )
