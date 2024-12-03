00d033c647ab533bcf93c44b4cdcd6b6b752e941
jogaudard <joseph.gaudard@pm.me>
2024-12-03 16:40:32 +0100

    created files for functions, need to turn script into functions

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ library(tarchetypes)
 
 # Set target options:
 tar_option_set(
-  packages = c("dataDownloader", "dataDocumentation", "tidyverse", "readxl", "lubridate", "writexl", "tibble", "stringi", "janitor"))
+  packages = c("dataDownloader", "dataDocumentation", "tidyverse", "readxl", "lubridate", "writexl", "tibble", "stringi", "janitor", "fluxible"))
 
 # tar_make_clustermq() configuration (okay to leave alone):
 options(clustermq.scheduler = "multicore")
