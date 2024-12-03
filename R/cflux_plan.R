fb376c3d5ec911016c1d0ba50e02bc2b62e913ac
jogaudard <joseph.gaudard@pm.me>
2024-12-03 11:56:22 +0100

    download 2020

--- R/cflux_plan.R
+++ R/cflux_plan.R
@@ -1,5 +1,12 @@
 # cflux plan
 
 cflux_plan <- list(
-    
+  tar_target(
+    name = cflux2020_download,
+    command = get_file(node = "pk4bg",
+                       file = "Three-D_cflux_2020.zip",
+                       path = "data/C-Flux/summer_2020",
+                       remote_path = "RawData/C-Flux"),
+    format = "file"
+  ),
 )
