ab7b587f0a502ff4f38bd929ce7329b7fabe5a16
audhalbritter <me@audhalbritter.com>
2025-06-10 13:49:14 +0200

    fix reflectance data

--- R/reflectance_plan.R
+++ R/reflectance_plan.R
@@ -6,27 +6,27 @@ reflectance_plan <- list(
   tar_target(
     name = ndvi20_download,
     command = get_file(node = "pk4bg",
-                       file = "THREE-D_NDVI_2020_aud.csv",
+                       file = "5_THREE-D_NDVI_2020_aud.csv",
                        path = "data",
-                       remote_path = "RawData/Vegetation"),
+                       remote_path = "RawData"),
     format = "file"
   ),
   
   tar_target(
     name = ndvi20_2_download,
     command = get_file(node = "pk4bg",
-                       file = "THREE-D_NDVI_2020_joseph.csv",
+                       file = "5_THREE-D_NDVI_2020_joseph.csv",
                        path = "data",
-                       remote_path = "RawData/Vegetation"),
+                       remote_path = "RawData"),
     format = "file"
   ),
   
   tar_target(
     name = ndvi22_download,
     command = get_file(node = "pk4bg",
-                       file = "THREE-D_NDVI_2022_joseph.csv",
+                       file = "5_THREE-D_NDVI_2022_joseph.csv",
                        path = "data",
-                       remote_path = "RawData/Vegetation"),
+                       remote_path = "RawData"),
     format = "file"
   ),
   
@@ -56,6 +56,7 @@ reflectance_plan <- list(
   tar_target(
     name = ndvi_out,
     command = save_csv(ndvi_clean,
+                       nr = "5_",
                        name = "reflectance_2020_2022")
   )
   
