32535f58cd04d6c0dfae30d9a8231435f41b556c
audhalbritter <me@audhalbritter.com>
2025-06-10 17:42:53 +0200

    add output

--- R/root_plan.R
+++ R/root_plan.R
@@ -8,7 +8,7 @@ root_plan <- list(
     command = get_file(node = "pk4bg",
                        file = "6_ThreeD_rootingrowthcores_2021.xlsx",
                        path = "data",
-                       remote_path = "RawData/Vegetation"),
+                       remote_path = "RawData"),
     format = "file"
   ),
   
@@ -18,7 +18,7 @@ root_plan <- list(
     command = get_file(node = "pk4bg",
                        file = "6_Three-D_raw_root_productivity_2022.xlsx",
                        path = "data",
-                       remote_path = "RawData/Vegetation"),
+                       remote_path = "RawData"),
     format = "file"
   ),
   
@@ -27,7 +27,7 @@ root_plan <- list(
     command = get_file(node = "pk4bg",
                        file = "6_Three-D_raw_root_traits_2022.txt",
                        path = "data",
-                       remote_path = "RawData/Vegetation"),
+                       remote_path = "RawData"),
     format = "file"
   ),
   
@@ -51,6 +51,14 @@ root_plan <- list(
   tar_target(
     name = roots_clean,
     command = clean_roots(root_productivity21_raw, decom_meta_raw, root_productivity22_raw, root_traits_raw, metaTurfID)
+  ),
+
+    # save data
+  tar_target(
+    name = roots_out,
+    command = save_csv(roots_clean,
+                       nr = "6_",
+                       name = "roots_2020_2022")
   )
   
   
