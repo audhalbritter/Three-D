c251bc88936fbbd48d80f34f309b9b1272933470
audhalbritter <me@audhalbritter.com>
2025-06-16 09:01:03 +0200

    add second productivity data

--- R/productivity_plan.R
+++ R/productivity_plan.R
@@ -31,6 +31,167 @@ productivity_plan <- list(
     command =  save_csv(productivity_clean,
                         nr = "4_",
                         name = "productivity_2022")
+  ),
+
+  # Productivity and environmental data from master thesis
+
+  # download productivity2
+  tar_target(
+    name = productivity2_download,
+    command = get_file(node = "pk4bg",
+                       file = "4_Three-D_raw_productivity_abiotic_2019.xlsx",
+                       path = "data",
+                       remote_path = "RawData"),
+    format = "file"
+  ),
+  
+  # import
+  tar_target(
+    name = productivity_fg_raw,
+    # warmings because there are NAs in date_in
+    command = read_excel(path = productivity2_download, sheet = "productivity")
+  ),
+
+  tar_target(
+    name = productivity_sp_raw,
+    # warmings because there are NAs in date_in
+    command = read_excel(path = productivity2_download, sheet = "productivtiy_sp")
+  ),
+
+  tar_target(
+    name = ph_raw,
+    # warmings because there are NAs in date_in
+    command = read_excel(path = productivity2_download, sheet = "pH")
+  ),
+
+  tar_target(
+    name = soilmoisture_raw,
+    # warmings because there are NAs in date_in
+    command = read_excel(path = productivity2_download, sheet = "soil moisture")
+  ),
+  
+  # import and clean data
+  tar_target(
+    name = productivity_fg_clean,
+    command = {
+
+    prod <- productivity_fg_raw |> 
+      janitor::clean_names() |> 
+      # join pH data
+      tidylog::left_join(ph_raw |> 
+        janitor::clean_names() |> 
+        select(-x3), by = "plot_id") |> 
+      mutate(date = dmy(date),
+      # distinguish permanent and movable plots, extract campaign nr 
+            type = str_extract(plot_id, "(\\d+|P)$"),
+            type = if_else(is.na(type), "1", type),
+            campaign = as.numeric(if_else(type %in% c("1", "2", "3"), type, NA_character_)),
+            type = if_else(type == "P", "permanent", "temporary"),
+            plot_id2 = str_remove(plot_id, "(\\d+|P)$")) |> 
+      extract(plot_id2,
+        into = c("siteID", "replicate", "treatment"),
+        regex = "^([A-Z])([0-9]+)(Cage|C)",
+        remove = FALSE) |> 
+
+      # remove 2 extra sites
+      filter(! siteID %in% c("H", "B")) |> 
+      mutate(siteID = case_when(siteID == "V" ~ "Vikesland",
+                                siteID == "J" ~ "Joasete",
+                                siteID == "L" ~ "Liahovden",
+                                TRUE ~ siteID),
+             treatment = if_else(treatment == "C", "Control", treatment),
+             replicate = as.numeric(replicate)) |> 
+      pivot_longer(cols = c(graminoids:shrubs), names_to = "functional_group", values_to = "biomass_g") |> 
+      tidylog::filter(!is.na(biomass_g)) |> 
+      select(date, campaign, siteID, plot_id, plot_id2, replicate, type, treatment:biomass_g, pH = p_h)
+      
+      # fix soil moisture data
+      sm <- soilmoisture_raw |> 
+        janitor::clean_names() |> 
+        # remove 2 extra sites
+        filter(!site %in% c("Hogsete", "In between")) |>
+        mutate(date = dmy(date),
+              across(c(m1, m2, m3, m4), as.numeric),
+            # distinguish permanent and movable plots, extract campaign nr 
+            type = str_extract(plot_id, "(\\d+|P)$"),
+            type = if_else(is.na(type), "1", type),
+            campaign = as.numeric(if_else(type %in% c("1", "2", "3"), type, NA_character_)),
+            type = if_else(type == "P", "permanent", "temporary"),
+            plot_id2 = str_remove(plot_id, "(\\d+|P)$"),
+          # fix wrong site
+            site = if_else(site == "Joesete", "Joasete", site)) |> 
+        rename(siteID = site,
+                date_sm = date) |> 
+        pivot_longer(cols = c(m1:m4), names_to = "sm_replicate", values_to = "soilmoisture") |> 
+        mutate(sm_replicate = as.numeric(str_remove(sm_replicate, "m"))) |> 
+        tidylog::select(date_sm, campaign, siteID, plot_id, plot_id2, type, treatment, sm_replicate, soilmoisture, weather, recorder, comments)
+      
+      # join soilmoisture to productivity data
+      prod |>  
+        tidylog::left_join(sm, 
+          by = c("campaign", "siteID", "plot_id", "plot_id2", "type", "treatment"))
+
+      
+    }
+      
+  ),
+
+  tar_target(
+    name = productivity_sp_clean,
+    command = {
+
+      prod_sp <- productivity_sp_raw |> 
+        clean_names() |> 
+        filter(!site %in% c("Hogsete", "In between")) |>
+        mutate(date = dmy(date),
+                # distinguish permanent and movable plots, extract campaign nr 
+                type = str_extract(plot_id, "(\\d+|P)$"),
+                type = if_else(is.na(type), "1", type),
+                campaign = as.numeric(if_else(type %in% c("1", "2", "3"), type, NA_character_)),
+                type = if_else(type == "P", "permanent", "temporary"),
+                plot_id2 = str_remove(plot_id, "(\\d+|P)$")) |> 
+        extract(plot_id2,
+            into = c("siteID", "replicate", "treatment"),
+            regex = "^([A-Z])([0-9]+)(Cage|C)",
+            remove = FALSE) |> 
+        mutate(treatment = if_else(treatment == "C", "Control", treatment),
+             replicate = as.numeric(replicate)) |> 
+        pivot_longer(cols = c(agrostis_capillaris:pinguicula_vulgaris), 
+                     names_to = "species", values_to = "biomass_g") |> 
+        tidylog::filter(!is.na(biomass_g)) |> 
+        select(date, campaign, siteID = site, plot_id, type, treatment, replicate, species, biomass_g)
+      
+      prod_sp |> 
+        mutate(species = case_when(species == "c_nigra" ~ "carex_nigra",
+                                    species == "carex_small_bigelowii" ~ "carex_sp1",
+                                    species == "carex_sp" ~ "carex_sp1",
+                                    species == "carex_yellow" ~ "carex_sp3",
+                                    species == "ranunculus_repens_32" ~ "ranunculus_repens",
+                                    species == "ranunculus_repens_66" ~ "ranunculus_repens",
+                                    species == "rubus_idaeus_2" ~ "rubus_idaeus",
+                                    species == "s_aizoides" ~ "saxifraga_aizoides",
+                                    species == "t_cespitosum" ~ "unknown_rush",
+                                    species == "vac_mytilus" ~ "vaccinium_mytilus",
+                                    species == "vac_uliginosum" ~ "vaccinium_uliginosum",
+                                    species == "vac_vitis_idea" ~ "vaccinium_vitis_idea",
+                                  TRUE ~ species))  
+
+    }
+  ),
+
+  # save data
+  tar_target(
+    name = productivity_fg_out,
+    command =  save_csv(productivity_fg_clean,
+                        nr = "4_",
+                        name = "productivity_fg_2019")
+  ),
+
+  tar_target(
+    name = productivity_sp_out,
+    command =  save_csv(productivity_sp_clean,
+                        nr = "4_",
+                        name = "productivity_sp_2019")
   )
   
 )
