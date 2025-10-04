27412e0ef925c9a2788868238539a739896d1881
audhalbritter <me@audhalbritter.com>
2025-10-04 16:09:19 +0200

    update flux plan

--- R/cflux_plan.R
+++ R/cflux_plan.R
@@ -4,41 +4,41 @@ cflux_plan <- list(
   tar_target(
     name = cflux2020_download,
     command = get_file(node = "pk4bg",
-                       file = "Three-D_cflux_2020.zip",
+                       file = "xiii_Three-D_cflux_2020.zip",
                        path = "data/C-Flux/summer_2020",
-                       remote_path = "RawData/13_Three-D_raw_C-Flux"),
+                       remote_path = "xiii_raw_co2_fluxes"),
     format = "file"
   ),
   tar_target(
     name = cfluxrecord2020_download,
     command = get_file(node = "pk4bg",
-                       file = "Three-D_field-record_2020.csv",
+                       file = "xiii_Three-D_field-record_2020.csv",
                        path = "data/C-Flux/summer_2020",
-                       remote_path = "RawData/13_Three-D_raw_C-Flux"),
+                       remote_path = "xiii_raw_co2_fluxes"),
     format = "file"
   ),
   tar_target(
     name = cflux2021_download,
     command = get_file(node = "pk4bg",
-                       file = "Three-D_cflux_2021.zip",
+                       file = "xiii_Three-D_cflux_2021.zip",
                        path = "data/c-flux/summer_2021",
-                       remote_path = "RawData/13_Three-D_raw_C-Flux"),
+                       remote_path = "xiii_raw_co2_fluxes"),
     format = "file"
   ),
   tar_target(
     name = cfluxrecord2021_download,
     command = get_file(node = "pk4bg",
-                       file = "Three-D_field-record_2021.csv",
+                       file = "xiii_Three-D_field-record_2021.csv",
                        path = "data/c-flux/summer_2021",
-                       remote_path = "RawData/13_Three-D_raw_C-Flux"),
+                       remote_path = "xiii_raw_co2_fluxes"),
     format = "file"
   ),
   tar_target(
     name = soilRchambersize_download,
     command = get_file(node = "pk4bg",
-                       file = "Three-D_soilR-chambers-size.csv",
+                       file = "xiii_Three-D_soilR-chambers-size.csv",
                        path = "data/c-flux/summer_2021",
-                       remote_path = "RawData/13_Three-D_raw_C-Flux"),
+                       remote_path = "xiii_raw_co2_fluxes"),
     format = "file"
   ),
   tar_target(
